7a8b859f1cf9c8fd9f1f2b1fe17a224b45bcbba4
JosiaAlmeida <josiaalmeida0001@gmail.com>
2022-02-22 22:07:56 +0100

    removendo classe desnecessaria

--- Movimento.java
+++
@@ -1,9 +0,0 @@
-package classes;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Objects;
-import java.util.Set;
-
-public class Movimento {
-    private Set<ContaCorrente> contacorrente = new LinkedHashSet<>();
-}
