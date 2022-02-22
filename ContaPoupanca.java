33332cf085ec976abc60b59f9ced6361bc406fd3
JosiaAlmeida <josiaalmeida0001@gmail.com>
2022-02-22 22:06:11 +0100

    Versao 2

---
+++ ContaPoupanca.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class ContaPoupanca extends Conta {
+
+    public ContaPoupanca(double numeroDeConta, Cliente nome, double saldo, double limite) {
+        super(numeroDeConta, nome, saldo, limite);
+    }
+
+    @Override
+    public void aplicaTaxa(double rendimento) {
+        taxa = rendimento * 3;
+    }
+    
+}
---
+++ Movimento.java
@@ -0,0 +1,9 @@
+package classes;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Movimento {
+    private Set<ContaCorrente> contacorrente = new LinkedHashSet<>();
+}
